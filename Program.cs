dc9d6bf78737386a6db08dbb7c4164c11e61ce4e
Caleb Kolman <ckolman@usf.edu>
2021-08-31 21:31:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kolman_TM5_TimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            //Count Starting From 1
+            int OrderCounter = 1;
+            while (length >= OrderCounter)
+            {
+                Console.WriteLine(OrderCounter.ToString() + " " + "Abracadabra");
+                OrderCounter++;
+            }
+        }
+    }
+}
+
